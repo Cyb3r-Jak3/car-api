ee9cd8b53b594275e8e83614a9446c46377cb634
Jacob White <jake@jwhite.network>
2022-05-02 17:01:50 -0400

    Add form clearing and error alerting

--- app/static/script.js
+++ app/static/script.js
@@ -1,12 +1,18 @@
 window.onload = function () {
-const apiForm = document.getElementById('apiForm');
+    const apiForm = document.getElementById('apiForm');
 
-apiForm.onsubmit = async (e) => {
-    e.preventDefault();
-    await fetch("/api/submit", {
-        method: 'POST',
-        body: new FormData(apiForm)
+    apiForm.onsubmit = async (e) => {
+        e.preventDefault();
+        const resp = await fetch("/api/submit", {
+            method: 'POST',
+            body: new FormData(apiForm)
+            }
+        );
+        if (resp.status !== 200) {
+            // const responseJson =
+            alert((await resp.json()).error)
+        } else {
+            apiForm.reset()
         }
-    );
-}
+    }
 };
