7ad575fc41aa3bd0b09752a8459aa323948bbed4
Jacob White <jake@jwhite.network>
2022-05-05 16:53:42 -0400

    Add more endpoint tests

--- tests/test_flask.py
+++ tests/test_flask.py
@@ -1,3 +1,5 @@
+import os
+
 import pytest
 from app import app
 
@@ -12,18 +14,25 @@ def test_index(client):
     assert resp.status_code == 200
 
 
+def test_range(client):
+    resp = client.get("/range")
+    assert resp.status_code == 200
+
+
 def test_bad_range(client):
     resp = client.post(
         "/api/submit",
         data={"BatteryPercentage": 100}
     )
     assert resp.status_code == 400
+    # assert resp.json["success"] is False
 
     resp = client.post(
         "/api/submit",
         data={"BatteryRange": 100}
     )
     assert resp.status_code == 400
+    # assert resp.json["success"] is False
 
 
 def test_good_range(client):
@@ -32,3 +41,68 @@ def test_good_range(client):
         data={"BatteryPercentage": 100, "BatteryRange": 100}
     )
     assert resp.status_code == 200
+    # assert resp.json["success"]
+
+
+def test_good_trip(client):
+    resp = client.post(
+        "/api/submit",
+        data={"miles": 10, "kwh": 3.5, "time": "0:15", "destination": "Flask Test"}
+    )
+    assert resp.status_code == 200
+    # assert resp.json["success"]
+
+
+def test_bad_trip(client):
+    resp = client.post(
+        "/api/submit",
+        data={"kwh": 3.5, "time": "0:15", "destination": "Flask Test"}
+    )
+    assert resp.status_code == 400
+    # assert resp.json["success"] is False
+
+    resp = client.post(
+        "/api/submit",
+        data={"miles": 10, "time": "0:15", "destination": "Flask Test"}
+    )
+    assert resp.status_code == 400
+    # assert resp.json["success"] is False
+
+    resp = client.post(
+        "/api/submit",
+        data={"miles": 10, "kwh": 3.5, "destination": "Flask Test"}
+    )
+    assert resp.status_code == 400
+    # assert resp.json["success"] is False
+
+    resp = client.post(
+        "/api/submit",
+        data={"miles": 10, "kwh": 3.5, "time": "0:15"}
+    )
+    assert resp.status_code == 400
+    # assert resp.json["success"] is False
+
+
+def test_good_charge(client):
+    resp = client.post(
+        "/api/submit",
+        data={"ChargeTime": "1:15:00", "ChargeAmount": 11.78}
+    )
+    assert resp.status_code == 200
+    # assert resp.json["success"]
+
+
+def test_bad_charge(client):
+    resp = client.post(
+        "/api/submit",
+        data={"ChargeAmount": 11.78}
+    )
+    assert resp.status_code == 400
+    # assert resp.json["success"] is False
+
+    resp = client.post(
+        "/api/submit",
+        data={"ChargeTime": "1:15:00", }
+    )
+    assert resp.status_code == 400
+    # assert resp.json["success"] is False
